9272ac1bdf359836a3365901b3259ab48c78fa65
Farrukh Sultani <farrukh.sultani@gmail.com>
2023-01-09 22:19:26 -0800

    started working on the actual code

--- Py_Bank/main.py
+++ Py_Bank/main.py
@@ -1,1 +1,29 @@
+#Read csv file
+import csv
+csvpath = /Users/farrukh/Documents/Documents - Farrukh’s MacBook Pro/GitHub/Farrukh-project/Python/Python_Challenge/Instructions 8/PyBank/Resources/budget_data.csv
+open(csvpath) as csv file:
+# printing headers
+print("Total Months: ")
+print("Total: ")
+print("Average Change: ")
+print("Greatest Increase in Profits: ")
+print("Greatest Decrease in Profits: ")
+
+#defining variables
+Total_months = ([month_counts])
+month_counts = 0
+Total = (sum(B2:B87))
+Average_Change = 0
+date = append[""]
+Greatest Increase in Profits = {increase["month"]} ${increase["profits"]}
+Greatest Decrease in Profits = {decrease["month"]} ${decrease["losses"]}
+#The total number of months included in the dataset
+
+#The net total amount of "Profit/Losses" over the entire period
+
+#The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+#The greatest increase in profits (date and amount) over the entire period
+
+#The greatest decrease in profits (date and amount) over the entire period
 
