349a9acebc01329f28d3336f317aece46e7bf475
Farrukh Sultani <farrukh.sultani@gmail.com>
2023-02-02 18:46:09 -0800

    Final code for Py_Poll

--- Py_Poll/main.py
+++ Py_Poll/main.py
@@ -2,7 +2,7 @@
 import os
 import csv
 csvpath = os.path.join("Resources", "election_data.csv")
-output = open('Analysis/Election_result.txt')
+output = open('Analysis/Election_result.txt', 'w')
 print(csvpath)
 #Define variables
 votecounts = 0
@@ -20,49 +20,45 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"Header:{csv_header}")
     output.write(f"Header:{csv_header}\n")
-    first_row = next(csvreader)
-    votecounts+=1
-    totalvotecounts+=int(first_row[0])
-    previous = int(first_row[1])
-
- 
- # A complete list of candidates who received votes
-    candidates_name = ['Charles Casper Stockham', 'Diana DeGette', 'Raymon Anthony Doane']
-    print(candidates_name)
-
 # The total number of votes each candidate won
     for row in csvreader:
-        if row[2] not in candidates_name:
-            candidates_name.append(row[2])
-            wincounts[row[2]]=0
-            wincounts[row[2]]+=1
+        votecounts += 1
+        totalvotecounts += 1
+        county = row[1]
+        candidate = row[2]
+#complete list of candidates 
+        if candidate not in candidates_name:
+            candidates_name.append(candidate)
+            wincounts[candidate] = 0
+        wincounts[candidate] += 1
 
-#Print
+#Print result
 print(f"Election Results")
 print(f"-------------------------")
-print(f"Total Votes: {votecounts}")
+print(f"Total Votes: {totalvotecounts}")
 print(f"-------------------------")
 output.write(f"Election Results\n")
 output.write(f"-------------------------\n")
-output.write(f"Total Votes: {votecounts}\n")
+output.write(f"Total Votes: {totalvotecounts}\n")
 output.write(f"-------------------------\n")
 
-# The percentage of votes each candidate won
-for  candidates_name in wincounts:
-    votes = wincounts[candidates_name]
-    winpercent = votes/votecounts
-    pretty = winpercent * 100
-    print(f"{candidates_name}: {pretty:.f}%({votes})")
-output.write(f"{candidates_name}:{pretty:.3f}%({votes})\n")
+# The percentage of votes each candidate won/winpercent
+for candidate in wincounts:
+    votes = wincounts[candidate]
+    winpercent = votes/totalvotecounts
+    pretty_percent = winpercent * 100
+    print(f"{candidate}: {pretty_percent:.3f}%({votes})")
+    output.write(f"{candidate}: {pretty_percent:.3f}%({votes})\n")
 
 # The winner of the election based on popular vote
-if votes > winnervotecounts: 
-    winnervotecounts = votes
-    winnername = candidates_name
+for candidate in wincounts:
+    if wincounts[candidate] > winnervotecounts:
+        winnervotecounts = wincounts[candidate]
+        winner_name = candidate
 
 print(f"----------------------------------------")
-print(f"Winner: {winnername}")
+print(f"Winner: {winner_name}")
 print(f"----------------------------------------")
-output.write(f"-------------------------\n")
-output.write(f"Winner: {winnername}\n")
-output.write(f"-------------------------\n")
+output.write(f"----------------------------------------\n")
+output.write(f"Winner: {winner_name}\n")
+output.write(f"----------------------------------------\n")
